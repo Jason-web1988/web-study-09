ace4459508623ec6aa882a637e2fd7244cd5c5fd
lenovo <lenovo@DESKTOP-CIIGDBG>
2020-08-27 12:26:27 +0900

    db테스트 완료

---
+++ test/web_study_09/ds/JdbcUtilJNDITest.java
@@ -0,0 +1,20 @@
+package web_study_09.ds;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JdbcUtilJNDITest {
+
+	@Test
+	public void testGetConnection() {
+		Connection con = JdbcUtilJNDI.getConnection();
+		Assert.assertNotNull(con);
+		System.out.println("con : " + con);
+	}//여기서는 테스트가 안됨
+
+
+}
